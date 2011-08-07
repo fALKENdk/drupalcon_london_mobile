93e09f3a356bbc8550885c886fa284ac92712ab8
Kasper Christensen <kasper@friischristensen.com>
2011-08-07 12:45:42 +0200

    My Schedule functionality

--- DrupalCon/Resources/drupalcon/drupalcon.js
+++ DrupalCon/Resources/drupalcon/drupalcon.js
@@ -47,10 +47,81 @@ var DrupalCon = {
   Ti.addEventListener('drupal:entity:datastore:update_completed', function(e) {
     presenterList = {};
   });
+	
+	DrupalCon.util.getDayName = function(date) {
+		date = date.split('T',1);
+		
+		switch(date[0]){
+			case '2011-08-22':
+				date = 'Monday 22nd August'
+				break;
+			case '2011-08-23':
+				date = 'Tuesday 23rd August'
+				break;
+			case '2011-08-24':
+				date = 'Wednesday 24th August'
+				break;
+			case '2011-08-25':
+				date = 'Thursday 25th August'
+				break;
+			case '2011-08-26':
+				date = 'Friday 26th August'
+				break;
+			default:
+				date = ''
+				break;
+		}
+		
+		return date;
+	};
+	
+	DrupalCon.util.myScheduleIsAdded = function(nid) {
+		isAdded = false;
+		var rows = Drupal.db.getConnection('main').query('SELECT nid FROM schedule WHERE nid = ' + nid);
+		
+    while (rows.isValidRow()) {
+      isAdded = true;
+      rows.next();
+    }
+    rows.close();
+    
+    return isAdded;
+	};
+	
+	DrupalCon.util.myScheduleIsBooked = function(start_date) {
+		var isBooked = false;
+		var rows = Drupal.db.getConnection('main').query('SELECT nid FROM schedule WHERE start_date = "'+start_date+'"');
+		
+		while (rows.isValidRow()) {
+      isBooked = true;
+      rows.next();
+    }
+    rows.close();
+    
+    return isBooked;
+	};
+	
+	DrupalCon.util.myScheduleSessionName = function(start_date) {
+		var sessionName;
+		var rows = Drupal.db.getConnection('main').query('SELECT nid FROM schedule WHERE start_date LIKE "'+start_date+'%"');
 
-
+    while (rows.isValidRow()) {
+    	var nid = rows.fieldByName('nid');
+    	var nodeRows = Drupal.db.getConnection('main').query('SELECT title FROM node WHERE nid = "' + nid + '"');
+     	while (nodeRows.isValidRow()){
+     		var sessionName = nodeRows.fieldByName('title');
+     		nodeRows.next();
+     	}
+     	nodeRows.close();
+      rows.next();
+    }
+    rows.close();
+    
+    return sessionName;
+	};
+	
   var lastTime = '';
-
+	
   DrupalCon.renderers.session = function(session) {
     var sessionTitle = cleanSpecialChars(session.title);
 
@@ -305,7 +376,105 @@ var DrupalCon = {
 
     return sessionRow;
   };
+	
+	DrupalCon.renderers.myschedule = function(session) {
+		Ti.API.info(session);
+		var sessionRow = Ti.UI.createTableViewRow({
+      hasChild:true,
+      backgroundColor: '#fff',
+      color: '#333',
+      start_date: session.start_date,
+      end_date: session.end_date,
+      nid: session.nid,
+      sessionTitle: 'sessionTitle',
+      itemType: session.type,
+      height: 'auto',
+      layout: 'vertical',
+      focusable : true,
+      borderWidth: 0,
+      backgroundSelectedImage: 'images/bgs/sitebuildingActive.png'
+      //headerView : cleanTime(session.start_date) + ' - ' + cleanTime(session.end_date)
+    });
+		
+		var leftSpace = (Ti.Platform.name == 'android') ? 10 : 25;
+    var titleColor = '#333';
+    var sessionColor;
 
+    switch (session.track) {
+      case "":
+        sessionRow.backgroundImage = 'images/bgs/default.png';
+        break;
+      case "Site Building and Environment Set-up":
+        sessionRow.backgroundImage = 'images/bgs/sitebuilding.png';
+        break;
+      case "Design, UX and Theming":
+        sessionRow.backgroundImage =  'images/bgs/design.png';
+        break;
+      case "Day Stage":
+        sessionRow.backgroundImage = 'images/bgs/day.png';
+        break;
+      case "Business and Best Practices":
+        sessionRow.backgroundImage =  'images/bgs/business.png';
+        break;
+      case "Ecosystem":
+        sessionRow.backgroundImage =  'images/bgs/ecosystem.png';
+        break;
+      case "Business Day":
+        sessionRow.backgroundImage =  'images/bgs/buisnessday.png';
+        break;
+      case "Code &amp; Coders":
+        sessionRow.backgroundImage =  'images/bgs/code.png';
+        break;
+      case "Core Conversations":
+        sessionRow.backgroundImage =  'images/bgs/core.png';
+        break;
+      default:
+        sessionRow.backgroundImage = 'images/bgs/default.png';
+        break;
+    }
+		
+		var titleLabel = Ti.UI.createLabel({
+      text: session.title,
+      font: {fontSize:16, fontWeight:'bold'},
+      color: titleColor,
+      left: leftSpace,
+      top: 10,
+      right: 10,
+      height: 'auto'
+    });
+    
+    // Some sessions have multiple presenters
+    var presLabel = Ti.UI.createLabel({
+      text: cleanSpecialChars(session.instructors.map(DrupalCon.util.getPresenterName).join(', ')),
+      font: {fontSize:13, fontWeight:'normal'},
+      color: '#000',
+      left: leftSpace,
+      top: 4,
+      bottom: 0,
+      right: 10,
+      height: 'auto'
+    });
+    
+    // Some things, like keynote, have multiple rooms
+    var roomLabel = Ti.UI.createLabel({
+      text: session.room.map(cleanSpecialChars).join(', '),
+      font: {fontSize:13, fontWeight:'normal', fontStyle:'italic'},
+      color: '#666',
+      left: leftSpace,
+      top: 1,
+      bottom: 10,
+      right: 10,
+      height: 'auto'
+    });
+
+    sessionRow.add(titleLabel);
+    sessionRow.add(presLabel);
+    sessionRow.add(roomLabel);
+
+    return sessionRow;
+    
+	};
+	
   DrupalCon.renderers.schedule_item = function(session) {
     var sessionTitle = cleanSpecialChars(session.title);
 		var specificRoom = (session.room.map(cleanSpecialChars).join(', ') != '') ? true : false;
---
+++ DrupalCon/Resources/windows/MyScheduleWindow.js
@@ -0,0 +1,134 @@
+/**
+ * This file is part of DrupalCon Mobile.
+ *
+ * DrupalCon Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DrupalCon Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrupalCon Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This is the page of My Schedule session listings.
+ */
+(function() {
+	var uiEnabled = true;
+	
+	DrupalCon.ui.createMyScheduleWindow = function(settings) {
+		Drupal.setDefaults(settings, {
+			title: 'title here',
+			start_date: '',
+			end_date: '',
+			tabGroup: undefined
+		});
+		
+		var myScheduleWindow = Titanium.UI.createWindow({
+			id: 'myScheduleWindow',
+			title: settings.title,
+			backgroundColor: '#FFF',
+			tabGroup: settings.tabGroup
+		});
+		
+		var data = [];
+		
+		var conn = Drupal.db.getConnection('main');
+		var rows = conn.query("SELECT nid FROM schedule ORDER BY start_date, nid");
+		
+		var nids = [];
+    while(rows.isValidRow()) {
+      nids.push(rows.fieldByName('nid'));
+      rows.next();
+    }
+    rows.close();
+
+		var sessions = Drupal.entity.db('main', 'node').loadMultiple(nids, ['start_date', 'nid']);
+		var dayName;
+		
+    for (var sessionNum = 0, numSessions = sessions.length; sessionNum < numSessions; sessionNum++) {
+      if (DrupalCon.renderers[sessions[sessionNum].type]) {
+      	sessions[sessionNum].type = 'myschedule';
+      	
+      	if(dayName = '' || dayName != DrupalCon.util.getDayName(sessions[sessionNum].start_date)) {
+      		dayName = DrupalCon.util.getDayName(sessions[sessionNum].start_date);
+      		
+      		var dateSection = Ti.UI.createTableViewSection();
+					var dateSectionView = Ti.UI.createView({height:'auto', backgroundColor: '#e4e0dd', borderColor: '#bfbcba', borderWidth: 1});
+ 
+					var dateSectionLabel = Ti.UI.createLabel({
+					    top:8, bottom:8, left:15, right:19,
+					    height:'auto',
+					    text:dayName,
+					    font:{fontSize:14, fontWeight:'bold'},
+					    color:'#333',
+					    shadowColor:'#FAFAFA',
+					    shadowOffset:{x:0, y:1},
+					});
+					 
+					dateSectionView.add(dateSectionLabel);
+					dateSection.headerView = dateSectionView;
+					
+    			data.push(dateSection);
+      	}
+      	
+      	var timeSection = Ti.UI.createTableViewSection();
+				var timeSectionView = Ti.UI.createView({height:'auto', backgroundColor: '#fbf7f3', borderColor: '#e0e0e0', borderWidth: 1});
+ 
+				var timeSectionLabel = Ti.UI.createLabel({
+				    top:8, bottom:8, left:15, right:19,
+				    height:'auto',
+				    text: cleanTime(sessions[sessionNum].start_date) + ' - ' + cleanTime(sessions[sessionNum].end_date),
+				    font:{fontSize:14, fontWeight:'bold'},
+				    color:'#333',
+				    shadowColor:'#FAFAFA',
+				    shadowOffset:{x:0, y:1},
+				});
+				 
+				timeSectionView.add(timeSectionLabel);
+				timeSection.headerView = timeSectionView;
+					
+      	data.push(timeSection);
+        data.push(DrupalCon.renderers['myschedule'](sessions[sessionNum]));
+      }
+      else {
+        Ti.API.info('Not rendering for node type: ' + sessions[sessionNum].type);
+      }
+    }
+		
+		// create table view
+    var tableview = Titanium.UI.createTableView({
+      data: data,
+      backgroundColor: '#fff',
+      layout:'vertical',
+      borderWidth: 0,
+      separatorStyle: 0,
+    });
+    
+    myScheduleWindow.addEventListener('focus', function() {
+      uiEnabled = true;
+    });
+
+    // Create table view event listener.
+    tableview.addEventListener('click', function(e) {
+      if (uiEnabled) {
+        uiEnabled = false;
+        var currentTab = (Ti.Platform.name == 'android') ? currentTab = Titanium.UI.currentTab : myScheduleWindow.tabGroup.activeTab;
+        currentTab.open(DrupalCon.ui.createSessionDetailWindow({
+          title: e.rowData.sessionTitle,
+          nid: e.rowData.nid,
+          tabGroup: currentTab
+        }), {animated:true});
+      }
+    });
+
+    myScheduleWindow.add(tableview);
+		
+		return myScheduleWindow;
+	};
+})();
